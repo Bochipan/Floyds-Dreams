db8de98b5ba275a449a2bdcacd3aced416e71886
Bochipan <115567023+Bochipan@users.noreply.github.com>
2025-02-13 20:23:32 +0100

    Ya camina

--- game/Floyd's Dreams/Assets/Scripts/EightWayMovement.cs
+++ game/Floyd's Dreams/Assets/Scripts/EightWayMovement.cs
@@ -7,7 +7,7 @@ public class EightWayMovement : MonoBehaviour
 
     public float speed = 5f; // Movement speed
     public float diagonalAngle = -30f; // Angle for diagonal movement (in degrees)
-    public Animator animator;
+    private Animator animator;
 
     private Vector2 movementInput;
     private Vector2 diagonal = - new Vector2(Mathf.Cos(30*Mathf.Rad2Deg), Mathf.Sin(30 * Mathf.Rad2Deg)).normalized;
@@ -23,20 +23,18 @@ public class EightWayMovement : MonoBehaviour
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("x", movementInput.x);
+        animator.SetFloat("y", movementInput.y);
+
         // Calculate movement direction based on input
         Vector2 direction = new Vector2(movementInput.x, movementInput.y);
 
         // Adjust diagonal direction
         if (direction.x != 0 && direction.y != 0)
         {
-   
             direction = diagonal * direction;
-            
         }
 
-        //Do we flip the character animation?
-
-        //this.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
 
         // Move the character
         transform.position += (Vector3)direction * speed * Time.deltaTime;
