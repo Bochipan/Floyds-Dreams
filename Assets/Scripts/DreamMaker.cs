8044875f78a40ccd0568d2392eecb39918f6f00a
Bochipan <sara.ber.ara@gmail.com>
2025-06-02 13:52:25 +0200

    cosas

--- Assets/Scripts/DreamMaker.cs
+++ Assets/Scripts/DreamMaker.cs
@@ -14,21 +14,31 @@ public class DreamMaker : MonoBehaviour
     public GameObject dream2;
     public GameObject floyd;
     public GameObject counter;
+    public Material halftone;
+    public Material halftone1;
+    public Material restaurant;
 
     void Start()
     {
         fade.StartCoroutine(fade.FadeTo(0f, 5f));
 
-        if (!GameManager.Instance.choices[0]) { 
+        if (GameManager.Instance.choices[0]) { 
 
             RenderSettings.skybox = mat1; 
             light1.color = new Color(1f, 0.964f, 0.693f);
             light2.color = new Color(1f, 0.662f, 0.486f);
+            halftone.SetColor("_BaseColor", new Color(1f, 1f, 1f));
+            halftone1.SetColor("_BaseColor", new Color(1f, 1f, 1f));
+            restaurant.SetColor("_BaseColor", new Color(1f, 1f, 1f));
+
         }
         else {
             RenderSettings.skybox = mat2;
             light1.color = new Color(0.41f, 0.355f, 0.717f);
             light2.color = new Color(0.434f, 0.227f, 0.428f);
+            halftone.SetColor("_BaseColor", new Color(0.59f, 0.60f, 1f));
+            halftone1.SetColor("_BaseColor", new Color(0.59f, 0.60f, 1f));
+            restaurant.SetColor("_BaseColor", new Color(0.59f, 0.60f, 1f));
         }
         if (GameManager.Instance.choices[2])
         {
